9a058ea95ec6219914e05a8dbdec3bc7a023b326
Turánszki János <turanszkij@users.noreply.github.com>
2023-09-21 08:43:58 +0200

    font renderer improvements (merge fix)

--- Crucible/wiVersion.cpp
+++ Crucible/wiVersion.cpp
@@ -12,7 +12,7 @@ namespace wi::version
 	// minor features, major updates, breaking compatibility changes
 	const int minor = 71;
 	// minor bug fixes, alterations, refactors, updates
-	const int revision = 291;
+	const int revision = 292;
 	#if defined(CRUCIBLE_NIGHTLY)
 	const std::string version_string = std::string("nightly_") + kGitHash + std::string("-") + kTimestamp;
 	#else
