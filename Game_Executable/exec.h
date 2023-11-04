30ad13e900020d06395095d0387197d8de1bddb1
Sothanforax <sothanforax@gmail.com>
2023-11-04 14:37:04 -0600

    Welder initial commit

---
+++ Game_Executable/exec.h
@@ -0,0 +1,203 @@
+#pragma once
+#include "wiScene_BindLua.h"
+#include "IconDefinitions.h"
+#include <map>
+
+class GameLoadingScreen : public wi::LoadingScreen
+{
+private:
+	wi::Sprite sprite;
+	wi::SpriteFont font;
+public:
+	void Load() override;
+	void Update(float dt) override;
+};
+
+class GameApp;
+
+class GameComponent : public wi::RenderPath2D
+{
+public:
+	GameApp* main = nullptr;
+
+	//wi::gui::Button newSceneButton;
+
+	//wi::gui::Button translateButton;
+	//wi::gui::Button rotateButton;
+	//wi::gui::Button scaleButton;
+
+	//wi::gui::Button dummyButton;
+	//bool dummy_enabled = false;
+	//bool dummy_male = false;
+	//XMFLOAT3 dummy_pos = XMFLOAT3(0, 0, 0);
+
+	//wi::gui::Button playButton;
+	//wi::gui::Button stopButton;
+
+	//wi::gui::Button saveButton;
+	//wi::gui::Button openButton;
+	//wi::gui::Button logButton;
+	//wi::gui::Button profilerButton;
+	//wi::gui::Button cinemaButton;
+	//wi::gui::Button fullscreenButton;
+	//wi::gui::Button bugButton;
+	//wi::gui::Button aboutButton;
+	//wi::gui::Button exitButton;
+	//wi::gui::Window aboutWindow;
+	//wi::gui::Label aboutLabel;
+
+	//OptionsWindow optionsWnd;
+	//ComponentsWindow componentsWnd;
+	//ProfilerWindow profilerWnd;
+
+	std::unique_ptr<wi::RenderPath3D> renderPath;
+	//const wi::graphics::Texture* GetGUIBlurredBackground() const override { return renderPath->GetGUIBlurredBackground(); }
+
+	//void ResizeBuffers() override;
+	//void ResizeLayout() override;
+	//void Load() override;
+	//void Start() override;
+	//void PreUpdate() override;
+	//void FixedUpdate() override;
+	//void Update(float dt) override;
+	//void PostUpdate() override;
+	//void Render() const override;
+	//void Compose(wi::graphics::CommandList cmd) const override;
+
+
+	enum EDITORSTENCILREF
+	{
+		EDITORSTENCILREF_CLEAR = 0x00,
+		EDITORSTENCILREF_HIGHLIGHT_OBJECT = 0x01,
+		EDITORSTENCILREF_HIGHLIGHT_MATERIAL = 0x02,
+		EDITORSTENCILREF_LAST = 0x0F,
+	};
+	wi::graphics::Texture rt_selectionOutline_MSAA;
+	wi::graphics::Texture rt_selectionOutline[2];
+	wi::graphics::Texture rt_dummyOutline;
+	float outlineTimer = 0;
+	const XMFLOAT4 selectionColor = XMFLOAT4(1, 0.6f, 0, 1);
+	const XMFLOAT4 selectionColor2 = XMFLOAT4(0, 1, 0.6f, 0.35f);
+	wi::Color inactiveEntityColor = wi::Color::fromFloat4(XMFLOAT4(1, 1, 1, 0.5f));
+	wi::Color hoveredEntityColor = wi::Color::fromFloat4(XMFLOAT4(1, 1, 1, 1));
+	wi::Color backgroundEntityColor = wi::Color::Black();
+	wi::Color dummyColor = wi::Color::White();
+
+	wi::graphics::Texture editor_depthbuffer;
+
+	//Translator translator;
+	wi::scene::PickResult hovered;
+	bool inspector_mode = false;
+	wi::ecs::Entity grass_interaction_entity = wi::ecs::INVALID_ENTITY;
+
+//	void ClearSelected();
+//	void AddSelected(wi::ecs::Entity entity);
+//	void AddSelected(const wi::scene::PickResult& picked);
+//	bool IsSelected(wi::ecs::Entity entity) const;
+	bool selectAll = false;
+	wi::unordered_set<wi::ecs::Entity> selectAllStorage;
+
+	bool bone_picking = false;
+	bool fpscamToggle = false;
+	void CheckBonePickingEnabled();
+
+	void UpdateTopMenuAnimation();
+
+
+
+	wi::Archive clipboard;
+
+	enum HistoryOperationType
+	{
+		HISTORYOP_TRANSLATOR,		// translator interaction
+		HISTORYOP_SELECTION,		// selection changed
+		HISTORYOP_ADD,				// entity added
+		HISTORYOP_DELETE,			// entity removed
+		HISTORYOP_COMPONENT_DATA,	// generic component data changed
+		HISTORYOP_PAINTTOOL,		// paint tool interaction
+		HISTORYOP_NONE
+	};
+
+//	void RecordSelection(wi::Archive& archive) const;
+//	void RecordEntity(wi::Archive& archive, wi::ecs::Entity entity);
+//	void RecordEntity(wi::Archive& archive, const wi::vector<wi::ecs::Entity>& entities);
+
+//	void ResetHistory();
+	wi::Archive& AdvanceHistory();
+//	void ConsumeHistoryOperation(bool undo);
+
+//	void Open(const std::string& filename);
+//	void Save(const std::string& filename);
+//	void SaveAs();
+	bool deleting = false;
+
+	std::string save_text_message = "";
+	std::string save_text_filename = "";
+	float save_text_alpha = 0; // seconds until save text disappears
+	wi::Color save_text_color = wi::Color::White();
+//	void PostSaveText(const std::string& message, const std::string& filename = "", float time_seconds = 4);
+
+	std::string last_script_path;
+
+	struct EditorScene
+	{
+		std::string path;
+		wi::scene::Scene scene;
+		XMFLOAT3 cam_move = {};
+		wi::scene::CameraComponent camera;
+		wi::scene::TransformComponent camera_transform;
+		wi::scene::TransformComponent camera_target;
+		wi::vector<wi::Archive> history;
+		int historyPos = -1;
+		//wi::gui::Button tabSelectButton;
+		//wi::gui::Button tabCloseButton;
+	};
+	wi::vector<std::unique_ptr<EditorScene>> scenes;
+	int current_scene = 0;
+	EditorScene& GetCurrentEditorScene() { return *scenes[current_scene].get(); }
+	const EditorScene& GetCurrentEditorScene() const { return *scenes[current_scene].get(); }
+	wi::scene::Scene& GetCurrentScene() { return scenes[current_scene].get()->scene; }
+	const wi::scene::Scene& GetCurrentScene() const { return scenes[current_scene].get()->scene; }
+//	void SetCurrentScene(int index);
+//	void RefreshSceneList();
+//	void NewScene();
+
+//	void FocusCameraOnSelected();
+
+	wi::Localization default_localization;
+	wi::Localization current_localization;
+//	void SetDefaultLocalization();
+//	void SetLocalization(wi::Localization& loc);
+//	void ReloadLanguage();
+
+	struct FontData
+	{
+		std::string name;
+		wi::vector<uint8_t> filedata;
+	};
+	wi::vector<FontData> font_datas;
+};
+
+class GameApp : public wi::Application
+{
+public:
+
+	static GameApp& getInstance() {
+		static GameApp instance;  // This instance is created lazily and only once
+		return instance;
+	}
+	GameComponent renderComponent;
+	GameLoadingScreen loader;
+	wi::config::File config;
+	wi::config::File gameconfig;
+
+	void Initialize() override;
+
+
+
+	~GameApp()
+	{
+		config.Commit();
+	}
+
+};
---
+++ Game_Executable/main.cpp
@@ -0,0 +1,192 @@
+#include "stdafx.h"
+#include "exec.h"
+#include "wiRenderer.h"
+#include <SDL2/SDL.h>
+#include "Assets/Icon.c"
+
+int sdl_loop(GameApp &gameapp)
+{
+    SDL_Event event;
+
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_PumpEvents();
+        gameapp.Run();
+
+        while( SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_WINDOWEVENT:
+                    switch (event.window.event)
+                    {
+                    case SDL_WINDOWEVENT_CLOSE:
+                        quit = true;
+                        break;
+                    case SDL_WINDOWEVENT_RESIZED:
+                        gameapp.SetWindow(gameapp.window);
+                        break;
+                    default:
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+
+    return 0;
+
+}
+
+void GameApp::Initialize()
+{
+
+	if (config.Has("font"))
+	{
+		// Replace default font from config:
+		wi::font::AddFontStyle(config.GetText("font"));
+	}
+
+	Application::Initialize();
+
+	// With this mode, file data for resources will be kept around. This allows serializing embedded resource data inside scenes
+	wi::resourcemanager::SetMode(wi::resourcemanager::Mode::ALLOW_RETAIN_FILEDATA);
+
+    infoDisplay.active = true;
+    infoDisplay.watermark = true;
+    infoDisplay.resolution = true;
+    infoDisplay.fpsinfo = true;
+	infoDisplay.heap_allocation_counter = true;
+	infoDisplay.pipeline_count = true;
+	infoDisplay.vram_usage = true;
+
+	wi::backlog::setFontColor(wi::Color(255, 0, 0, 255));
+
+	wi::renderer::SetOcclusionCullingEnabled(true);
+
+	loader.Load();
+
+	renderComponent.main = this;
+
+	loader.addLoadingComponent(&renderComponent, this, 0.2f);
+
+	ActivatePath(&loader);
+
+}
+
+void GameLoadingScreen::Load()
+{
+	font = wi::SpriteFont("Loading...", wi::font::Params(0, 0, 36, wi::font::WIFALIGN_LEFT, wi::font::WIFALIGN_CENTER));
+	font.anim.typewriter.time = 2;
+	font.anim.typewriter.looped = true;
+	font.anim.typewriter.character_start = 7;
+	AddFont(&font);
+
+	sprite.anim.opa = 1;
+	sprite.anim.repeatable = true;
+	sprite.params.siz = XMFLOAT2(128, 128);
+	sprite.params.pivot = XMFLOAT2(0.5f, 1.0f);
+	sprite.params.quality = wi::image::QUALITY_LINEAR;
+	sprite.params.blendFlag = wi::enums::BLENDMODE_ALPHA;
+	AddSprite(&sprite);
+
+	wi::gui::CheckBox::SetCheckTextGlobal(ICON_CHECK);
+
+	LoadingScreen::Load();
+}
+void GameLoadingScreen::Update(float dt)
+{
+	font.params.posX = GetLogicalWidth()*0.5f - font.TextWidth() * 0.5f;
+	font.params.posY = GetLogicalHeight()*0.5f;
+	sprite.params.pos = XMFLOAT3(GetLogicalWidth()*0.5f, GetLogicalHeight()*0.5f - font.TextHeight(), 0);
+	sprite.textureResource.SetTexture(*wi::texturehelper::getLogo()); // use embedded asset
+
+	LoadingScreen::Update(dt);
+}
+
+void set_window_icon(SDL_Window *window) {
+    // these masks are needed to tell SDL_CreateRGBSurface(From)
+    // to assume the data it gets is byte-wise RGB(A) data
+    Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    int shift = (gimp_image.bytes_per_pixel == 3) ? 8 : 0;
+    rmask = 0xff000000 >> shift;
+    gmask = 0x00ff0000 >> shift;
+    bmask = 0x0000ff00 >> shift;
+    amask = 0x000000ff >> shift;
+#else // little endian, like x86
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = (gimp_image.bytes_per_pixel == 3) ? 0 : 0xff000000;
+#endif
+    SDL_Surface* icon = SDL_CreateRGBSurfaceFrom((void*)gimp_image.pixel_data, gimp_image.width,
+        gimp_image.height, gimp_image.bytes_per_pixel*8, gimp_image.bytes_per_pixel*gimp_image.width,
+        rmask, gmask, bmask, amask);
+
+    SDL_SetWindowIcon(window, icon);
+
+    SDL_FreeSurface(icon);
+}
+
+int main(int argc, char *argv[])
+{
+    #ifdef WickedEngine_SHADER_DIR
+    wi::renderer::SetShaderSourcePath(WickedEngine_SHADER_DIR);
+    #endif
+
+int width = 1920;
+int height = 1080;
+bool fullscreen = false;
+wi::Timer timer;
+
+	if (GameApp::getInstance().config.Open("config.ini"))
+	{
+		if (GameApp::getInstance().config.Has("width"))
+		{
+			width = GameApp::getInstance().config.GetInt("width");
+			height = GameApp::getInstance().config.GetInt("height");
+		}
+		fullscreen = GameApp::getInstance().config.GetBool("fullscreen");
+		GameApp::getInstance().allow_hdr = GameApp::getInstance().config.GetBool("allow_hdr");
+
+		wi::backlog::post("config.ini loaded in " + std::to_string(timer.elapsed_milliseconds()) + " milliseconds\n");
+	}
+
+	width = std::max(100, width);
+	height = std::max(100, height);
+
+    sdl2::sdlsystem_ptr_t system = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING | SDL_INIT_EVENTS);
+    sdl2::window_ptr_t window = sdl2::make_window(
+            "Welder",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            width, height,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI);
+	if (!window) {
+    	throw sdl2::SDLError("Error creating window");
+    }
+
+    SDL_Event event;
+
+	set_window_icon(window.get());
+
+	if (fullscreen)
+	{
+		//SDL_SetWindowFullscreen(window.get(), SDL_TRUE);
+		//SDL_SetWindowFullscreen(window.get(), SDL_WINDOW_FULLSCREEN);
+		SDL_SetWindowFullscreen(window.get(), SDL_WINDOW_FULLSCREEN_DESKTOP);
+	}
+
+	GameApp::getInstance().SetWindow(window.get());
+
+    int ret = sdl_loop(GameApp::getInstance());
+
+    SDL_Quit();
+
+    return ret;
+}
